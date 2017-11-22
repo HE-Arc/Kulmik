1e137600fd5f9768fcd271ca46277eb2df985447
Julia Németh <cptnwing@gmail.com>
2017-11-22 19:34:24 +0100

    expiration date check.

--- resources/views/expired_food_list.blade.php
+++ resources/views/expired_food_list.blade.php
@@ -1,7 +1,69 @@
+@php
+
+    $aliments = \App\Aliment::all();
+
+@endphp
+
+
 @section('expired_food')
 
-    <!--TODO: case 1: show list of soon to be expired food (~3-5 days)-->
-    <!--TODO: case 2: show list of soon to be expired food (~6-10 days)-->
-    <!--TODO: case 3: "All is good"-->
+        @foreach($aliments as $aliment)
+            @if($aliment->hasExpired("expired") != null)
+                <h3>Has expired</h3>
+                <div class="row">
+                    <div class="col-sm-2">{{ $aliment->name }}</div>
+                    @if($aliment->description != "")
+                        <div class="col-sm-2">{{ $aliment->description}}</div>
+                    @endif
+                </div>
+            @endif
+        @endforeach
 
+        @foreach($aliments as $aliment)
+            @if($aliment->hasExpired("today") != null)
+                <h3>Expires today</h3>
+                <div class="row">
+                    <div class="col-sm-2">{{ $aliment->name }}</div>
+                    @if($aliment->description != "")
+                        <div class="col-sm-2">{{ $aliment->description}}</div>
+                    @endif
+                </div>
+            @endif
+        @endforeach
+
+        @foreach($aliments as $aliment)
+            @if($aliment->hasExpired("stage3") != null)
+                <h3>Expires in 2-1 days</h3>
+                <div class="row">
+                    <div class="col-sm-2">{{ $aliment->name }}</div>
+                    @if($aliment->description != "")
+                        <div class="col-sm-2">{{ $aliment->description}}</div>
+                    @endif
+                </div>
+            @endif
+        @endforeach
+
+        @foreach($aliments as $aliment)
+            @if($aliment->hasExpired("stage2") != null)
+                <h3>Expires in 3-5 days</h3>
+                <div class="row">
+                    <div class="col-sm-2">{{ $aliment->name }}</div>
+                    @if($aliment->description != "")
+                        <div class="col-sm-2">{{ $aliment->description}}</div>
+                    @endif
+                </div>
+            @endif
+        @endforeach
+
+        @foreach($aliments as $aliment)
+            @if($aliment->hasExpired("stage1") != null)
+                <h3>Expires in 6-10 days</h3>
+                <div class="row">
+                    <div class="col-sm-2">{{ $aliment->name }}</div>
+                    @if($aliment->description != "")
+                        <div class="col-sm-2">{{ $aliment->description}}</div>
+                    @endif
+                </div>
+            @endif
+        @endforeach
 @endsection
--- resources/views/pages/aliments/index.blade.php
+++ resources/views/pages/aliments/index.blade.php
@@ -5,6 +5,7 @@
     <h1>All your food is here</h1>
     Do you wan't to add an aliment : <button class="btn btn-xs btn-info">Add an aliment</button>
 
+    <!--TODO: refactor -->
     @foreach($categories as $category)
         <div class="container">
             <h2>{{$category->name}} <button class="btn btn-xs btn-info">+</button></h2>
@@ -12,18 +13,18 @@
                     @if($aliment->category_id == $category->id)
                     <div class="row">
                         <div class="col-sm-2">{{ $aliment->name }}</div>
-                        @if($category->id == 7)
+                        @if($aliment->description == "")
+                            <div class="col-sm-2">no description</div>
+                        @elseif($aliment->description != "")
                             <div class="col-sm-2">{{ $aliment->description}}</div>
                         @endif
-                        @if($category->id == 6 || $category->id == 7 || $category->id == 9)
-                            <div class="col-sm-1">{{ $aliment->weight}} [L]</div>
-                        @elseif($category->id != 6 && $category->id != 7 && $category->id != 9)
-                            <div class="col-sm-1">{{ $aliment->weight}} [g]</div>
-                        @endif
+                        <div class="col-sm-1">{{ $aliment->weight}}
+                            {{ ($category->name == "drinks" || $category->name == "alcohol" || $category->name == "sweet beverage" || $category->name == "juice") ? "[L]" : "[g]" }}</div>
                         <div class="col-sm-2"><b>quantity: </b>{{ $aliment->quantity}}</div>
                         <div class="col-sm-2"><b>bought on: </b>{{ $aliment->buy_date}}</div>
                         <div class="col-sm-2"><b>best until: </b>{{ $aliment->expiration_date}}</div>
-                        <div class="col-sm-1"><a href="{{ action('AlimentController@show', ['id' => $aliment->id]) }}" class="btn btn-xs btn-info">Edit</a></div>
+
+                        <div class="col-sm-1"><a href="{{ action('AlimentController@show', ['id' => $aliment->id]) }}" class="btn btn-xs btn-info">Edit</a></div
                         <div class="col-sm-1"><button class="btn btn-xs btn-info">Delete</button></div>
                     </div>
                     @endif
